5d509369cd020b6d0d8e7cea6b478bd629c0bdf5
Callum Dewsnap <70774692+Dewsnap@users.noreply.github.com>
2021-11-28 21:04:23 -0500

    Update testAtmosphere.py

--- testAtmosphere.py
+++ testAtmosphere.py
@@ -5,11 +5,12 @@ import pandas as pd
 
 from scipy import interpolate
 
-height = np.linspace(0, 10000, num=333)
+height = np.linspace(0, 10000, num=2000)
 
+# 1440 per day
 time = np.linspace(0, 24, 1440)
 
-atmos = np.zeros((25, 333))
+atmos = np.zeros((25, 2000))
 
 std = np.random.normal(200, 50, 1)
 mean = np.random.normal(2000, 200, 1)
@@ -45,7 +46,7 @@ g = sns.heatmap(df, cmap='jet')
 plt.gca().invert_yaxis()
 g.set_xticks(np.linspace(0,1440,25))
 g.set_xticklabels(['0:00','','','','','','6:00','','','','','','12:00','','','','','','18:00','','','','','','24:00'])
-g.set_yticks([0, 83.25, 165.5, 249.75, 333])
+g.set_yticks([0, 500, 1000, 1500, 2000])
 g.set_yticklabels([0, 2500, 5000, 7500, 10000])
 
 plt.savefig('/home/callum/Desktop/atmosphere.png')
